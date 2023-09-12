ad8a180d13bd7d21ba84895a5af6a9e637bfa00d
kleberaluizio <kleberaluizio@gmail.com>
2023-09-12 11:46:08 -0300

    Strategy implemented

--- store/src/com/kleberaluizio/Main.java
+++ store/src/com/kleberaluizio/Main.java
@@ -1,14 +1,17 @@
+package com.kleberaluizio;
+
 import com.kleberaluizio.budget.Budget;
+import com.kleberaluizio.tax.ICMS;
 import com.kleberaluizio.tax.ISS;
 import com.kleberaluizio.tax.TaxCalculator;
 
 import java.math.BigDecimal;
 
-public class TaxTests {
+public class Main {
     public static void main(String[] args) {
         Budget budget = new Budget(new BigDecimal("100"));
         TaxCalculator taxCalc = new TaxCalculator();
 
-        System.out.println(taxCalc.calculate(budget, new ISS()));
+        System.out.println(taxCalc.calculate(budget, new ICMS()));
     }
 }
