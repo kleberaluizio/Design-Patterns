35904baeaa5f2bd908bd91fc294fd549a76e758b
kleberaluizio <kleberaluizio@gmail.com>
2023-09-12 09:58:55 -0300

    Studying Strategy design pattern

---
+++ store/src/com/kleberaluizio/Budget.java
@@ -0,0 +1,16 @@
+package com.kleberaluizio;
+
+import java.math.BigDecimal;
+
+public class Budget {
+
+    private BigDecimal value;
+
+    public Budget(BigDecimal value) {
+        this.value = value;
+    }
+
+    public BigDecimal getValue(){
+        return value;
+    }
+}
